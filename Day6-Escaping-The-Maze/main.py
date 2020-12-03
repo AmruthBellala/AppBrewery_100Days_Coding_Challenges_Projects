2489cc8d551a0299da1693d81c6c8df716aed909
Amruth Bellala <43343459+AmruthBellala@users.noreply.github.com>
2020-12-03 10:16:21 +0530

    Update main.py

--- Day6-Escaping-The-Maze/main.py
+++ Day6-Escaping-The-Maze/main.py
@@ -4,14 +4,14 @@ def turn_right():
     turn_left()
     turn_left()
     turn_left()
-
+while front_is_clear():
+    move()
+turn_left()
 while not at_goal():
-    if front_is_clear() and (not right_is_clear()):
+    if right_is_clear():
+        turn_right()
         move()
-    else:
-        if right_is_clear():
-            turn_right()
-            move()
-            
-        else:    
-            turn_left()
+    elif front_is_clear():
+        move()
+    else:    
+        turn_left()
